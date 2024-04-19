262f7715e4df16054e99a20c33f1c206b85119b2
Darien Sessions <darien.sessions@gmail.com>
2024-04-19 10:52:10 -0400

    refactoring and adding functionality with JS

--- scripts/index.js
+++ scripts/index.js
@@ -26,14 +26,61 @@ let initialCards = [
   },
 ];
 
+//ELEMENTS
+
+const cardTemplate =
+  document.querySelector(".card__template").content.firstElementChild;
+const cardsList = document.querySelector(".cards__list");
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profileEditModal = document.querySelector(".modal");
 const modalCloseButton = document.querySelector(".modal__close");
+const editProfileName = document.querySelector(".modal__form-input--name");
+const editProfileSubtitle = document.querySelector(
+  ".modal__form-input--subtitle"
+);
+const profileName = document.querySelector(".profile__name");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+const profileEditForm = document.querySelector(".modal__form");
 
-profileEditButton.addEventListener("click", function () {
-  profileEditModal.classList.add("modal_opened");
+//FUNCTIONS
+
+function getCardElement(data) {
+  let cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+
+  cardTitle.textContent = data.name;
+  cardImage.alt = data.name;
+  cardImage.src = data.link;
+  return cardElement;
+}
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardsList.append(cardElement);
 });
 
-modalCloseButton.addEventListener("click", function () {
+//EVENT HANDLERS
+
+function handleClosePopup() {
   profileEditModal.classList.remove("modal_opened");
+}
+
+function handleProfileEditSubmit(e) {
+  e.preventDefault();
+  profileName.textContent = editProfileName.value;
+  profileSubtitle.textContent = editProfileSubtitle.value;
+  handleClosePopup();
+}
+
+//EVENT LISTENERS
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+  editProfileName.value = profileName.textContent;
+  editProfileSubtitle.value = profileSubtitle.textContent;
 });
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+modalCloseButton.addEventListener("click", handleClosePopup);
